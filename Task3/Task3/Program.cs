eba0e0ac925c12e664fb6d81f0952147884a8da2
radiatus <radiatus@yandex.ru>
2016-09-23 17:52:03 +0300

    2

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -10,6 +10,25 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            int[] massive;
+            int a, b, c;
+            for (a = 1;a<20;a++)
+            {
+                for (b = 1;b<20;b++)
+                {
+                    for (c = 1;c<20;c++)
+                    {
+                        if ((a*a + b*b == c*c))
+                        {
+                            Console.WriteLine("{0} {1} {2}",a,b,c);
+
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
         }
     }
+
+    //6.	Составить программу для нахождения и печати всех пифагоровых чисел, не превышающих 20.
 }
