a6e4f50338103afa9359f9092c2aaa3096b7c48b
radiatus <radiatus@yandex.ru>
2016-10-06 20:14:05 +0300

    Таск 5 ееееее

---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//9.	Дан массив размера N. Найти максимальный из его локальных минимумов
+
+namespace Task5
+{
+    class Program
+    {
+        static uint ReadUint()
+        {
+            uint nUint;
+            while (!UInt32.TryParse(Console.ReadLine(), out nUint))
+            {
+                Console.Write("Введите правильно: ");
+            }
+            return nUint;
+        }
+
+        static int Rand(Random rng)
+        {
+            return rng.Next(100);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Длина массива: ");
+            uint N = ReadUint();
+            uint[] mas = new uint[N];
+            Random rng = new Random();
+
+            for (uint i = 0;i<N;i++)
+            {
+                mas[i] = Convert.ToUInt32(Rand(rng));
+            }
+
+            foreach (uint count in mas)
+            {
+                Console.WriteLine(count);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
