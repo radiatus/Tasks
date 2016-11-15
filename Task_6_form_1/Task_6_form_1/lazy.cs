0c476f272769bc897d2ddedf3277da5dd9be1997
radiatus <radiatus@yandex.ru>
2016-11-15 10:46:20 +0300

    уауау

--- Task_6_form_1/Task_6_form_1/lazy.cs
+++ Task_6_form_1/Task_6_form_1/lazy.cs
@@ -9,19 +9,23 @@ namespace Task_6_form_1
 {
     class lazy
     {
-        public static bool M(DataGridView data, int row, int column)
+        public static bool M(DataGridView data, int row, int column) // Дописать для [i-1,j+1]
         {
             int countT = 0;
             
+            if (column != 0 && row != 0 && Convert.ToInt32(data[column - 1, row - 1].Value) == Convert.ToInt32(data[column, row].Value))
+                countT++;
             if (column != 0 && Convert.ToInt32(data[column - 1, row].Value) == Convert.ToInt32(data[column, row].Value))
                 countT++;
-            if (column != 0 && row != 0 && Convert.ToInt32(data[column - 1, row - 1].Value) == Convert.ToInt32(data[column, row].Value))
+            if (column != 0 && row != data.RowCount - 1 && row != 0 && Convert.ToInt32(data[column - 1, row + 1].Value) == Convert.ToInt32(data[column, row].Value))
                 countT++;
             if (row != 0 && Convert.ToInt32(data[column, row - 1].Value) == Convert.ToInt32(data[column, row].Value))
                 countT++;
-            if (column != data.ColumnCount - 1 && Convert.ToInt32(data[column + 1, row].Value) == Convert.ToInt32(data[column, row].Value))
-                countT++;
             if (row != data.RowCount - 1 && Convert.ToInt32(data[column, row + 1].Value) == Convert.ToInt32(data[column, row].Value))
+                countT++;
+            if (column != data.ColumnCount - 1 && row != 0 && Convert.ToInt32(data[column + 1, row - 1].Value) == Convert.ToInt32(data[column, row].Value))
+                countT++;
+            if (column != data.ColumnCount - 1 && Convert.ToInt32(data[column + 1, row].Value) == Convert.ToInt32(data[column, row].Value))
                 countT++;
             if (column != data.RowCount - 1 && row != data.ColumnCount - 1 && Convert.ToInt32(data[column + 1, row + 1].Value) == Convert.ToInt32(data[column, row].Value))
                 countT++;
