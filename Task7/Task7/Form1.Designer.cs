49a486d1b4bf2beda3ce729a70f27ffb2297d5c1
radiatus <radiatus@yandex.ru>
2016-11-25 11:00:14 +0300

    хрень

---
+++ Task7/Task7/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Task7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Number = new System.Windows.Forms.TextBox();
+            this.ConvertToText = new System.Windows.Forms.Button();
+            this.NumberText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Number
+            // 
+            this.Number.Location = new System.Drawing.Point(13, 13);
+            this.Number.Name = "Number";
+            this.Number.Size = new System.Drawing.Size(171, 20);
+            this.Number.TabIndex = 0;
+            // 
+            // ConvertToText
+            // 
+            this.ConvertToText.Location = new System.Drawing.Point(190, 10);
+            this.ConvertToText.Name = "ConvertToText";
+            this.ConvertToText.Size = new System.Drawing.Size(75, 23);
+            this.ConvertToText.TabIndex = 1;
+            this.ConvertToText.Text = "Convert";
+            this.ConvertToText.UseVisualStyleBackColor = true;
+            this.ConvertToText.Click += new System.EventHandler(this.ConvertToText_Click);
+            // 
+            // NumberText
+            // 
+            this.NumberText.Location = new System.Drawing.Point(12, 39);
+            this.NumberText.Name = "NumberText";
+            this.NumberText.Size = new System.Drawing.Size(566, 20);
+            this.NumberText.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 69);
+            this.Controls.Add(this.NumberText);
+            this.Controls.Add(this.ConvertToText);
+            this.Controls.Add(this.Number);
+            this.Name = "Form1";
+            this.Text = "Task 7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Number;
+        private System.Windows.Forms.Button ConvertToText;
+        private System.Windows.Forms.TextBox NumberText;
+    }
+}
+
---
+++ Task7/Task7/Functions.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task7
+{
+    class Functions
+    {
+        public static string ConvertNumberToText(string NumberString)
+        {
+            int Number = Convert.ToInt32(NumberString);
+
+            string NumberText = "";
+
+            int units = Number % 10;
+            int tens = (Number - units) % 100 / 10;
+            int hundreds = (Number - tens -units) % 1000 / 100;
+            int thousands = (Number - tens - units - hundreds) % 10000 / 1000;
+            int tensThousands = (Number - tens - units - hundreds - thousands) % 100000 / 10000;
+            int hundredsThousands = (Number - tens - units - hundreds - thousands - tensThousands) % 1000000 / 100000;
+            int millions = (Number - tens - units - hundreds - thousands - tensThousands - hundredsThousands) % 10000000 /1000000;
+
+            switch (millions)
+            {
+                case 1:
+                    NumberText += "Один миллион ";
+                    break;
+                case 2:
+                    NumberText += "Два миллиона ";
+                    break;
+                case 3:
+                    NumberText += "Три миллион ";
+                    break;
+                case 4:
+                    NumberText += "Четыре миллион ";
+                    break;
+                case 5:
+                    NumberText += "Пять миллионов ";
+                    break;
+                case 6:
+                    NumberText += "Шесть миллионов ";
+                    break;
+                case 7:
+                    NumberText += "Семь миллионов ";
+                    break;
+                case 8:
+                    NumberText += "Восемь миллионов ";
+                    break;
+                case 9:
+                    NumberText += "Девять миллионов ";
+                    break;
+            }
+
+            switch (hundredsThousands)
+            {
+                case 1:
+                    NumberText += "сто ";
+                    break;
+                case 2:
+                    NumberText += "двесте ";
+                    break;
+                case 3:
+                    NumberText += "триста ";
+                    break;
+                case 4:
+                    NumberText += "четыреста ";
+                    break;
+                case 5:
+                    NumberText += "пятьсот ";
+                    break;
+                case 6:
+                    NumberText += "шестьсот ";
+                    break;
+                case 7:
+                    NumberText += "семьсот ";
+                    break;
+                case 8:
+                    NumberText += "восемьсот";
+                    break;
+                case 9:
+                    NumberText += "девятьсот ";
+                    break;
+            }
+
+            switch (tensThousands)
+            {
+                case 1:
+                    NumberText += "десять ";
+                    break;
+                case 2:
+                    NumberText += "дватцать ";
+                    break;
+                case 3:
+                    NumberText += "тритцать ";
+                    break;
+                case 4:
+                    NumberText += "сорок ";
+                    break;
+                case 5:
+                    NumberText += "пятьдесят ";
+                    break;
+                case 6:
+                    NumberText += "шестьдесят ";
+                    break;
+                case 7:
+                    NumberText += "семьдесят ";
+                    break;
+                case 8:
+                    NumberText += "восемьдесят ";
+                    break;
+                case 9:
+                    NumberText += "девяносто ";
+                    break;
+            }
+
+            switch (thousands)
+            {
+                case 1:
+                    NumberText += "одна ";
+                    break;
+                case 2:
+                    NumberText += "две ";
+                    break;
+                case 3:
+                    NumberText += "три ";
+                    break;
+                case 4:
+                    NumberText += "четыре ";
+                    break;
+                case 5:
+                    NumberText += "пять ";
+                    break;
+                case 6:
+                    NumberText += "шесть ";
+                    break;
+                case 7:
+                    NumberText += "семьт ";
+                    break;
+                case 8:
+                    NumberText += "восемь ";
+                    break;
+                case 9:
+                    NumberText += "девять ";
+                    break;
+            }
+
+            if (thousands != 0 || tensThousands != 0 || hundredsThousands != 0)
+                NumberText += "тысяч ";
+            
+            switch (hundreds)
+            {
+                case 1:
+                    NumberText += "сто ";
+                    break;
+                case 2:
+                    NumberText += "двесте ";
+                    break;
+                case 3:
+                    NumberText += "триста ";
+                    break;
+                case 4:
+                    NumberText += "четыреста ";
+                    break;
+                case 5:
+                    NumberText += "пятьсот ";
+                    break;
+                case 6:
+                    NumberText += "шестьсот ";
+                    break;
+                case 7:
+                    NumberText += "семьсот ";
+                    break;
+                case 8:
+                    NumberText += "восемьсот";
+                    break;
+                case 9:
+                    NumberText += "девятьсот ";
+                    break;
+            }
+
+            switch (tens)
+            {
+                case 1:
+                    NumberText += "десять ";
+                    break;
+                case 2:
+                    NumberText += "дватцать ";
+                    break;
+                case 3:
+                    NumberText += "тритцать ";
+                    break;
+                case 4:
+                    NumberText += "сорок ";
+                    break;
+                case 5:
+                    NumberText += "пятьдесят ";
+                    break;
+                case 6:
+                    NumberText += "шестьдесят ";
+                    break;
+                case 7:
+                    NumberText += "семьдесят ";
+                    break;
+                case 8:
+                    NumberText += "восемьдесят ";
+                    break;
+                case 9:
+                    NumberText += "девяносто ";
+                    break;
+            }
+
+             switch (units)
+            {
+                case 1:
+                    NumberText += "один";
+                    break;
+                case 2:
+                    NumberText += "два";
+                    break;
+                case 3:
+                    NumberText += "три";
+                    break;
+                case 4:
+                    NumberText += "четыре";
+                    break;
+                case 5:
+                    NumberText += "пять";
+                    break;
+                case 6:
+                    NumberText += "шесть";
+                    break;
+                case 7:
+                    NumberText += "семь";
+                    break;
+                case 8:
+                    NumberText += "восемь";
+                    break;
+                case 9:
+                    NumberText += "девять";
+                    break;
+            }
+            return NumberText;
+        }
+    }
+}
