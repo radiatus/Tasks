1f62877e4d8837e314c88c39c0c3b029cfd7270e
radiatus <radiatus@yandex.ru>
2016-12-05 17:45:23 +0300

    Сдать и забыть как страшный сон

--- Task7/Task7/Functions.cs
+++ Task7/Task7/Functions.cs
@@ -15,234 +15,100 @@ namespace Task7
 
             string NumberText = "";
 
-            int units = Number % 10;
-            int tens = (Number - units) % 100 / 10;
-            int hundreds = (Number - tens -units) % 1000 / 100;
-            int thousands = (Number - tens - units - hundreds) % 10000 / 1000;
-            int tensThousands = (Number - tens - units - hundreds - thousands) % 100000 / 10000;
-            int hundredsThousands = (Number - tens - units - hundreds - thousands - tensThousands) % 1000000 / 100000;
-            int millions = (Number - tens - units - hundreds - thousands - tensThousands - hundredsThousands) % 10000000 /1000000;
-
-            switch (millions)
+            int[] numbers = getNumbers(Number);
+                        
+            if (numbers[4] <= 1 && numbers[4] !=0 && numbers[3] != 0)
             {
-                case 1:
-                    NumberText += "Один миллион ";
-                    break;
-                case 2:
-                    NumberText += "Два миллиона ";
-                    break;
-                case 3:
-                    NumberText += "Три миллиона ";
-                    break;
-                case 4:
-                    NumberText += "Четыре миллиона ";
-                    break;
-                case 5:
-                    NumberText += "Пять миллионов ";
-                    break;
-                case 6:
-                    NumberText += "Шесть миллионов ";
-                    break;
-                case 7:
-                    NumberText += "Семь миллионов ";
-                    break;
-                case 8:
-                    NumberText += "Восемь миллионов ";
-                    break;
-                case 9:
-                    NumberText += "Девять миллионов ";
-                    break;
-            }
 
-            switch (hundredsThousands)
+                NumberText += get(numbers[5], 3);
+                NumberText += get(numbers[3], 4);
+            }
+            else
             {
-                case 1:
-                    NumberText += "сто ";
-                    break;
-                case 2:
-                    NumberText += "двести ";
-                    break;
-                case 3:
-                    NumberText += "триста ";
-                    break;
-                case 4:
-                    NumberText += "четыреста ";
-                    break;
-                case 5:
-                    NumberText += "пятьсот ";
-                    break;
-                case 6:
-                    NumberText += "шестьсот ";
-                    break;
-                case 7:
-                    NumberText += "семьсот ";
-                    break;
-                case 8:
-                    NumberText += "восемьсот ";
-                    break;
-                case 9:
-                    NumberText += "девятьсот ";
-                    break;
+                NumberText += get(numbers[5], 3);
+                NumberText += get(numbers[4], 2);
+                NumberText += get(numbers[3], 1);
             }
-
-            switch (tensThousands)
-            {
-                case 1:
-                    NumberText += "десять ";
-                    break;
-                case 2:
-                    NumberText += "двадцать ";
-                    break;
-                case 3:
-                    NumberText += "тридцать ";
-                    break;
-                case 4:
-                    NumberText += "сорок ";
-                    break;
-                case 5:
-                    NumberText += "пятьдесят ";
-                    break;
-                case 6:
-                    NumberText += "шестьдесят ";
-                    break;
-                case 7:
-                    NumberText += "семьдесят ";
-                    break;
-                case 8:
-                    NumberText += "восемьдесят ";
-                    break;
-                case 9:
-                    NumberText += "девяносто ";
-                    break;
-            }
-
-            switch (thousands)
-            {
-                case 1:
-                    NumberText += "одна ";
-                    break;
-                case 2:
-                    NumberText += "две ";
-                    break;
-                case 3:
-                    NumberText += "три ";
-                    break;
-                case 4:
-                    NumberText += "четыре ";
-                    break;
-                case 5:
-                    NumberText += "пять ";
-                    break;
-                case 6:
-                    NumberText += "шесть ";
-                    break;
-                case 7:
-                    NumberText += "семьт ";
-                    break;
-                case 8:
-                    NumberText += "восемь ";
-                    break;
-                case 9:
-                    NumberText += "девять ";
-                    break;
-            }
-
-            if (thousands != 0 || tensThousands != 0 || hundredsThousands != 0)
-                NumberText += "тысячи ";
             
-            switch (hundreds)
+            if (numbers[5] != 0 || numbers[4] != 0 || numbers[3] != 0)
+                NumberText += thousands(numbers[3]);
+            
+            if (numbers[1] <= 1 && numbers[1] != 0 && numbers[0] != 0)
             {
-                case 1:
-                    NumberText += "сто ";
-                    break;
-                case 2:
-                    NumberText += "двесте ";
-                    break;
-                case 3:
-                    NumberText += "триста ";
-                    break;
-                case 4:
-                    NumberText += "четыреста ";
-                    break;
-                case 5:
-                    NumberText += "пятьсот ";
-                    break;
-                case 6:
-                    NumberText += "шестьсот ";
-                    break;
-                case 7:
-                    NumberText += "семьсот ";
-                    break;
-                case 8:
-                    NumberText += "восемьсот";
-                    break;
-                case 9:
-                    NumberText += "девятьсот ";
-                    break;
+                NumberText += get(numbers[2], 3);
+                NumberText += get(numbers[0], 4);
             }
-
-            switch (tens)
+            else
             {
-                case 1:
-                    NumberText += "десять ";
-                    break;
-                case 2:
-                    NumberText += "двадцать ";
-                    break;
-                case 3:
-                    NumberText += "дридцать ";
-                    break;
-                case 4:
-                    NumberText += "сорок ";
-                    break;
-                case 5:
-                    NumberText += "пятьдесят ";
-                    break;
-                case 6:
-                    NumberText += "шестьдесят ";
-                    break;
-                case 7:
-                    NumberText += "семьдесят ";
-                    break;
-                case 8:
-                    NumberText += "восемьдесят ";
-                    break;
-                case 9:
-                    NumberText += "девяносто ";
-                    break;
+                NumberText += get(numbers[2], 3);
+                NumberText += get(numbers[1], 2);
+                NumberText += get(numbers[0], 0);
             }
-
-             switch (units)
-            {
-                case 1:
-                    NumberText += "один";
-                    break;
-                case 2:
-                    NumberText += "два";
-                    break;
-                case 3:
-                    NumberText += "три";
-                    break;
-                case 4:
-                    NumberText += "четыре";
-                    break;
-                case 5:
-                    NumberText += "пять";
-                    break;
-                case 6:
-                    NumberText += "шесть";
-                    break;
-                case 7:
-                    NumberText += "семь";
-                    break;
-                case 8:
-                    NumberText += "восемь";
-                    break;
-                case 9:
-                    NumberText += "девять";
-                    break;
-            }
+            
             return NumberText;
         }
+
+        public static string get(int number, int discarge)
+        {
+            string[][] units = new string[][]
+                {
+                 new string[] {"", "один ", "два ", "три ", "четыре ", "пять ", "шесть ", "семь ", "восемь ", "девять " },
+                 new string[] {"", "одна ","две ", "три ", "четыре ", "пять ", "шесть ", "семь ", "восемь ", "девять "},
+                 new string[] {"","десять ","двадцать ","тридцать ","сорок ","пятьдесят ", "шестьдесят ", "семьдесят ","восемьдесят ","дявяносто "},
+                 new string[] {"","сто ", "двести ", "триста ", "четыреста ", "пятьсот ", "шестьсот ", "семьсот ", "восемьсот ", "девятсот "},
+                 new string[] { "десять ","одинадцать ", "двенадцать ", "тринадцать ", "пятьнадцать ", "шестнадцать ", "семнадцать ", "восемнадцать ", "девятнадцать "}
+                };
+            
+            return units[discarge][number];
+        }
+
+        public static string thousands(int i)
+        {
+            switch (i)
+            {
+                case 1:
+                    return "тысяча ";
+                case 3:
+                    return "тысячи ";
+                case 4:
+                    return "тысячи ";
+                default:
+                    return "тысяч ";
+            }
+        }
+        public static int[] getNumbers(int number)
+        {
+            int[] numbers = new int[6];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = returnDischarge(number, i);
+            }
+
+            return numbers;
+        }
+
+        public static int returnDischarge(int number, int discharge)
+        {
+            switch (discharge)
+            {
+                case 0:
+                    return number % 10;
+                case 1:
+                    return number % 100 / 10;
+                case 2:
+                    return number % 1000 / 100;
+                case 3:
+                    return number % 10000 / 1000;
+                case 4:
+                    return number % 100000 / 10000;
+                case 5:
+                    return number % 1000000 / 100000;
+                default:
+                    MessageBox.Show("Разряда не существует");
+                    return 0;
+            }
+        }
+
     }
 }
